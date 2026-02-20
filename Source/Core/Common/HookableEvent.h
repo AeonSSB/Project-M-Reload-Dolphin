04da94bf6ac5761ed968fc929182a24a8c4c037c
Aeon <efisiomii@gmail.com>
2026-02-20 18:43:25 +0100

    Revert "Test"
    This reverts commit e7dd0a7900f0f882765b177d76301b867cad505f.

--- Source/Core/Common/HookableEvent.h
+++ Source/Core/Common/HookableEvent.h
@@ -88,7 +88,6 @@ public:
 private:
   struct Listener
   {
-    explicit Listener(CallbackType cb) : callback(std::move(cb)) {}
     const CallbackType callback;
     bool is_pending_removal{};
   };
